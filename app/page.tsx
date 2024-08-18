de9b6807d491bb3ace1571d0ae3b54d8b4606707
AHMAD M <111442059+Ahmad8426@users.noreply.github.com>
2024-08-18 23:12:05 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,20 @@ import { HeroCards } from "@/components/hero-card";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-  export default function Home() {
-    return (
-      <section className="container grid lg:grid-cols-2 -mt-28 place-items-center py-20 md:py-32 gap-10 bottom-4">
+export default function Home() {
+  return (
+    <section className="container grid lg:grid-cols-2 -mt-28 place-items-center py-20 md:py-32 gap-10 bottom-4">
       <div className="text-center h-screen lg:h-auto flex flex-col items-center lg:text-start space-y-6">
         <main className="text-5xl md:text-6xl flex flex-col items-center mt-40 lg:pt-8 font-bold">
           <h1 className="inline py-2">
             <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
               Welcome
             </span>{" "}
-            to my
-          </h1>{" "}
-          {" "}
-          <h2 className="inline">
-            <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-              Personal Portfolio
-            </span>{" "}
-          </h2>
+          </h1>
         </main>
 
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-        I am a researcher specializing in AI and Machine Learning, with expertise in Computer Vision and NLP.
+          I am a researcher specializing in AI and Machine Learning, with expertise in Computer Vision and NLP.
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
@@ -47,5 +40,5 @@ import Link from "next/link";
       {/* Shadow effect */}
       <div className="shadow"></div>
     </section>
-    );
-  }
+  );
+}
