234363e74824f187f51d2e2ca9cecf414c1ddde6
AHMAD M <111442059+Ahmad8426@users.noreply.github.com>
2024-08-19 10:56:19 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form/dist/useForm";
 import { isValidPhoneNumber } from "react-phone-number-input";
 import { z } from "zod";
 import { parsePhoneNumberFromString } from 'libphonenumber-js';
