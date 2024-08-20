873f4e0644cbe37123b3b21f8e3c156b226342a0
AHMAD M <111442059+Ahmad8426@users.noreply.github.com>
2024-08-20 10:51:26 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { FaAddressCard, FaPhoneAlt } from "react-icons/fa";
 import { MdAttachEmail } from "react-icons/md";
@@ -14,24 +14,22 @@ import { Input } from "@/components/ui/input";
 import { PhoneInput } from "@/components/ui/phone-input";
 import { useToast } from "@/components/ui/use-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { isValidPhoneNumber } from "react-phone-number-input";
 import { z } from "zod";
-import { parsePhoneNumberFromString } from 'libphonenumber-js';
 import { Textarea } from "@/components/ui/textarea";
 import Link from "next/link";
 import { Toaster } from "@/components/ui/toaster";
 
 const FormSchema = z.object({
-  name: z.string().min(2, {
-    message: "Username must be at least 2 characters."
-  }),
+  name: z.string().min(2, { message: "Name must be at least 2 characters long." }),
   phone: z.string().refine(isValidPhoneNumber, { message: "Invalid phone number" }),
   email: z.string().email({ message: "Invalid email address" }),
-  message: z.string().min(10, { message: "Message must be at least 10 characters." }),
+  message: z.string().min(10, { message: "Message must be at least 10 characters long." }),
 });
+
 export default function ContactPage() {
-  const form = useForm<z.infer<typeof FormSchema>>({
+  const form = useForm({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       name: "",
@@ -43,7 +41,7 @@ export default function ContactPage() {
 
   const { toast } = useToast();
 
-  async function onSubmit(data: z.infer<typeof FormSchema>) {
+  async function onSubmit(data) {
     try {
       const response = await fetch('api/contact', {
         method: 'POST',
@@ -79,46 +77,46 @@ export default function ContactPage() {
 
   return (
     <section className="lg:flex w-full justify-center items-cente p-4">
-<div className="lg:flex flex-col p-4 lg:items-center lg:mt-20 lg:border border-gray-500 rounded-r-none pt-20 lg:py-40 rounded-xl lg:w-1/3">
-      <span className="p-4 inline-block w-full bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-        <h1 className="text-3xl text-center font-bold">Contact</h1>
-      </span>
-      <div className="">
-        <p className="text-lg lg:flex items-center flex-wrap gap-4">
-          <span className="flex items-center inline-block gap-2">
-            <MdAttachEmail />
-            <span className="">
-              <Link href="mailto:ahmy40404@gmail.com" target="_blank" type="email">Email</Link>:
+      <div className="lg:flex flex-col p-4 lg:items-center lg:mt-20 lg:border border-gray-500 rounded-r-none pt-20 lg:py-40 rounded-xl lg:w-1/3">
+        <span className="p-4 inline-block w-full bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
+          <h1 className="text-3xl text-center font-bold">Contact</h1>
+        </span>
+        <div className="">
+          <p className="text-lg lg:flex items-center flex-wrap gap-4">
+            <span className="flex items-center inline-block gap-2">
+              <MdAttachEmail />
+              <span className="">
+                <Link href="mailto:ahmy40404@gmail.com" target="_blank" type="email">Email</Link>:
+              </span>
             </span>
-          </span>
-          ahmy40404@gmail.com
-        </p>
-        <p className="text-lg lg:flex items-center flex-wrap gap-4">
-          <span className="flex items-center inline-block gap-2">
-            <FaPhoneAlt />
-            <span className="">
-              <Link href="tel:+916382429579" target="_blank" type="tel">Phone</Link>:
+            ahmy40404@gmail.com
+          </p>
+          <p className="text-lg lg:flex items-center flex-wrap gap-4">
+            <span className="flex items-center inline-block gap-2">
+              <FaPhoneAlt />
+              <span className="">
+                <Link href="tel:+916382429579" target="_blank" type="tel">Phone</Link>:
+              </span>
             </span>
-          </span>
-          +91 63824 29579
-        </p>
-        <p className="text-lg lg:flex items-center flex-wrap gap-4">
-          <span className="flex items-center inline-block gap-2">
-            <FaAddressCard />
-            <span className="">
-              <Link href="" target="_blank">Address</Link>:
+            +91 63824 29579
+          </p>
+          <p className="text-lg lg:flex items-center flex-wrap gap-4">
+            <span className="flex items-center inline-block gap-2">
+              <FaAddressCard />
+              <span className="">
+                <Link href="" target="_blank">Address</Link>:
+              </span>
             </span>
-          </span>
-          Chennai, Tamil Nadu, India
-        </p>
+            Chennai, Tamil Nadu, India
+          </p>
+        </div>
       </div>
-    </div>
-    <div className="mt-10 lg:mt-20 border border-gray-500 lg:rounded-l-none p-10 rounded-xl lg:w-1/3">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-8 flex flex-col items-start"
-        >
+      <div className="mt-10 lg:mt-20 border border-gray-500 lg:rounded-l-none p-10 rounded-xl lg:w-1/3">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 flex flex-col items-start"
+          >
             <FormField
               control={form.control}
               name="name"
@@ -177,6 +175,6 @@ export default function ContactPage() {
           </form>
         </Form>
       </div>
-      </section>
-);
+    </section>
+  );
 }
